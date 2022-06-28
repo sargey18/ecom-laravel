9644c8481768ddb6fb42a17a436cb04d4481e2be
Ryan Sargeson <ryanteachviet@gmail.com>
2022-06-28 21:45:55 +0100

    first commit

---
+++ resources/views/backend/brand/brand_view.blade.php
@@ -0,0 +1,138 @@
+@extends('admin.admin_master')
+@section('admin')
+
+
+    <div class="container-full">
+      <!-- Content Header (Page header) -->
+
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="row">
+            
+
+
+          <div class="col-8">
+
+           <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title">Brand List</h3>
+              </div>
+              <!-- /.box-header -->
+              <div class="box-body">
+                  <div class="table-responsive">
+                    <table id="example1" class="table table-bordered table-striped">
+                      <thead>
+                          <tr>
+                              <th>Brand En</th>
+                              <th>Image</th>
+                              <th>Action</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+
+                          @foreach($brands as $item)
+                          <tr>
+                              <td>{{$item->brand_name_en}}</td>
+                              <td><img src="{{ asset($item->brand_image) }}" style="width: 70px; height: 40px";></td>
+                              <td>
+                                  <a href="{{ route('brand.edit', $item->id )}}" class="btn btn-info">Edit</a>
+                                  <a href="{{ route('brand.delete', $item->id )}}" class="btn btn-danger" id="delete">Delete</a>
+                              </td>
+                          </tr>
+                          @endforeach
+
+                      </tbody>
+
+                    </table>
+                  </div>
+              </div>
+              <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+
+
+            <!-- /.box -->          
+          </div>
+          <!-- /.col -->
+
+
+          <!-- add brand page -->
+          <div class="col-4">
+
+            <div class="box">
+               <div class="box-header with-border">
+                 <h3 class="box-title">Add Brand</h3>
+               </div>
+               <!-- /.box-header -->
+               <div class="box-body">
+                   <div class="table-responsive">
+                      <form method="post" action="{{route('brand.store')}}" enctype="multipart/form-data" >
+                        @csrf
+
+               
+  
+  
+                                  
+                          <div class="form-group">
+                              <h5>Brand Name English <span class="text-danger">*</span></h5>
+                              <div class="controls">
+                                  
+                                  <input type="text" name="brand_name_en" class="form-control"> 
+
+                                  @error('brand_name_en')
+                                  <span class="text-danger">{{$message}}</span>
+                                      
+                                  @enderror
+                                
+                                </div>
+                          </div>
+  
+
+  
+                          <div class="form-group">
+                              <h5>Brand Image <span class="text-danger">*</span></h5>
+                              <div class="controls">
+                                  <input type="file" id="password_confirmation" name="brand_image" class="form-control" value="Add New" > 
+                                  @error('brand_image')
+                                  <span class="text-danger">{{$message}}</span>
+                                      
+                                  @enderror
+                                
+                                </div>
+                          </div>
+  
+                              
+  
+                       
+  
+  
+  
+  
+  
+  
+                          <div class="text-xs-right">
+                              <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
+                          </div>
+                      </form>
+                   </div>
+               </div>
+               <!-- /.box-body -->
+             </div>
+             <!-- /.box -->
+ 
+ 
+             <!-- /.box -->          
+           </div>
+
+
+        </div>
+        <!-- /.row -->
+      </section>
+      <!-- /.content -->
+    
+    </div>
+
+
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\AdminProfileController;
+use App\Http\Controllers\Backend\BrandController;
+use App\Models\User;
+use App\Models\Auth;
+use App\Http\Controllers\Frontend\IndexController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['prefix'=> 'admin', 'middleware'=>['admin:admin']], function(){
+	Route::get('/login', [AdminController::class, 'loginForm']);
+	Route::post('/login',[AdminController::class, 'store'])->name('admin.login');
+});
+
+Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
+    return view('admin.index');
+})->name('dashboard');
+
+
+//admin logout
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+// admin profile
+Route::get('/admin/profile', [AdminProfileController::class, 'AdminProfile'])->name('admin.profile');
+// edit admin profile 
+Route::get('/admin/profile/edit', [AdminProfileController::class, 'AdminProfileEdit'])->name('admin.profile.edit');
+
+Route::post('/admin/profile/store', [AdminProfileController::class, 'AdminProfileStore'])->name('admin.profile.store');
+
+Route::get('/admin/change/password', [AdminProfileController::class, 'AdminChangePassword'])->name('admin.change.password');
+
+Route::post('/update/change/password', [AdminProfileController::class, 'AdminUpdateChangePassword'])->name('update.change.password');
+
+// user all route
+
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+    $id = Auth::user()->id;
+    $user = User::find($id);
+    return view('dashboard', compact('user'));
+})->name('dashboard');
+
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
+
+Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
+
+Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
+
+Route::get('/user/change/password', [IndexController::class, 'UserChangePassword'])->name('change.password');
+
+Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update');
+
+Route::prefix('brand')->group(function(){
+Route::get('/view', [BrandController::class, 'BrandView'])->name('all.brand');
+Route::post('/store', [BrandController::class, 'BrandStore'])->name('brand.store');
+Route::get('/edit/{id}', [BrandController::class, 'BrandEdit'])->name('brand.edit');
+Route::post('/update', [BrandController::class, 'BrandUpdate'])->name('brand.update');
+Route::get('/delete/{id}', [BrandController::class, 'BrandDelete'])->name('brand.delete');
+
+
+
+});
